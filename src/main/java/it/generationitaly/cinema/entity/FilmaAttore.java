928c8b10b823879eafb4c9ce08290bfccf978d95
Utente <Utente@PCLorena>
2024-10-10 17:42:50 +0200

    sistemato film attore

--- src/main/java/it/generationitaly/cinema/entity/FilmaAttore.java
+++ src/main/java/it/generationitaly/cinema/entity/FilmaAttore.java
@@ -1,14 +1,18 @@
 package it.generationitaly.cinema.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
-public class Film_attore {
+@Entity
+@Table(name = "film_attore")
+public class FilmaAttore {
 
 	 @Id
 	 @GeneratedValue(strategy = GenerationType.IDENTITY)
