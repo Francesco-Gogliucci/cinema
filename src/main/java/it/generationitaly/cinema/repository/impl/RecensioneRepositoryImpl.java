426c67f82be49cf659a789aa6f905b5fc395afcc
Francesco <fgogliucci@gmail.com>
2024-10-10 17:57:20 +0200

    aggiunto il costruttore

--- src/main/java/it/generationitaly/cinema/repository/impl/RecensioneRepositoryImpl.java
+++ src/main/java/it/generationitaly/cinema/repository/impl/RecensioneRepositoryImpl.java
@@ -5,4 +5,8 @@ import it.generationitaly.cinema.repository.RecensioneRepository;
 
 public class RecensioneRepositoryImpl extends JpaRepositoryImpl<Recensione, Long> implements RecensioneRepository {
 
+	public RecensioneRepositoryImpl() {
+		super(Recensione.class);
+	}
+
 }
