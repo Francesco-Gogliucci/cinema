2ae380155b47d2048e566d5e637625160b513eef
Francesco <fgogliucci@gmail.com>
2024-10-10 17:28:51 +0200

    Categoria repository e impl

---
+++ src/main/java/it/generationitaly/cinema/repository/CategoriaRepository.java
@@ -0,0 +1,7 @@
+package it.generationitaly.cinema.repository;
+
+import it.generationitaly.cinema.entity.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+}
--- src/main/java/it/generationitaly/cinema/repository/impl/FilmRepositoryImpl.java
+++ src/main/java/it/generationitaly/cinema/repository/impl/FilmRepositoryImpl.java
@@ -2,7 +2,6 @@ package it.generationitaly.cinema.repository.impl;
 
 import it.generationitaly.cinema.enity.Film;
 import it.generationitaly.cinema.repository.FilmRepository;
-import it.generationitaly.cinema.repository.JpaRepository;
 
 public class FilmRepositoryImpl extends JpaRepository<Film, Long> implements FilmRepository {
 	public FilmRepositoryImpl() {
