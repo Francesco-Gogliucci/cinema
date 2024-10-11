c8a45ff3d9c43672eb0e743b1f1121248b152713
Francesco <fgogliucci@gmail.com>
2024-10-11 12:44:21 +0200

    test

--- src/main/java/it/generationitaly/cinema/controller/maintest.java
+++ src/main/java/it/generationitaly/cinema/controller/maintest.java
@@ -1,8 +1,5 @@
 package it.generationitaly.cinema.controller;
 
-import java.util.List;
-
-import it.generationitaly.cinema.entity.Categoria;
 import it.generationitaly.cinema.repository.impl.AttoreRepositoryImpl;
 import it.generationitaly.cinema.repository.impl.CategoriaRepositoryImpl;
 import it.generationitaly.cinema.repository.impl.FilmAttoreRepositoryImpl;
@@ -21,36 +18,9 @@ public class maintest {
 		PreferitiRepositoryImpl preferiti = new PreferitiRepositoryImpl();
 		RecensioneRepositoryImpl recensione = new RecensioneRepositoryImpl();
 		UtenteRepositoryImpl utente = new UtenteRepositoryImpl();
-		/*
-		 * System.out.println("----------ATTORE----------"); List<Attore> listattore =
-		 * attore.findAll(); for (Attore attore2 : listattore) {
-		 * System.out.println(attore2); }
-		 */
-		System.out.println("----------CATEGORIA----------");
-		List<Categoria> listcategoria = categoria.findAll();
-		for (Categoria categoria2 : listcategoria) {
-			System.out.println(categoria2);
-		}
-		/*
-		 * System.out.println("----------FILMATTORE----------"); List<FilmAttore>
-		 * listFilmAttore = filmAttore.findAll(); for (FilmAttore filmAttore2 :
-		 * listFilmAttore) { System.out.println(filmAttore2); }
-		 * 
-		 * System.out.println("----------FILM----------"); List<Film> listFilm =
-		 * film.findAll(); for (Film film2 : listFilm) { System.out.println(film2); }
-		 * 
-		 * System.out.println("----------PREFERITI----------"); List<Preferiti>
-		 * listPreferiti = preferiti.findAll(); for (Preferiti preferiti2 :
-		 * listPreferiti) { System.out.println(preferiti2); }
-		 * 
-		 * System.out.println("----------RECENSIONE----------"); List<Recensione>
-		 * listRecensione = recensione.findAll(); for (Recensione recensione2 :
-		 * listRecensione) { System.out.println(recensione2); }
-		 * 
-		 * System.out.println("----------UTENTE----------"); List<Utente> listUtente =
-		 * utente.findAll(); for (Utente utente2 : listUtente) {
-		 * System.out.println(utente2); }
-		 */
+
+		System.out.println("test");
+
 	}
 
 }
