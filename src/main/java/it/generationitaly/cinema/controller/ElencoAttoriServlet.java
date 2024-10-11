b92c70c74300ae69aa44b66fa3bb8e4c013d1289
Francesco <fgogliucci@gmail.com>
2024-10-11 10:07:09 +0200

    elenco attori servlet

---
+++ src/main/java/it/generationitaly/cinema/controller/ElencoAttoriServlet.java
@@ -0,0 +1,40 @@
+package it.generationitaly.cinema.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import it.generationitaly.cinema.entity.Attore;
+import it.generationitaly.cinema.repository.impl.AttoreRepositoryImpl;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/elencoAttoriServlet")
+public class ElencoAttoriServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private AttoreRepositoryImpl attoreRepository = new AttoreRepositoryImpl();
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		List<Attore> elencoAttori = attoreRepository.findAll();
+
+		request.setAttribute("elencoAttori", elencoAttori);
+		// inserire la pagina jsp corretta per la visualizzazione della lista degli
+		// attori
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("elencoAttori.jsp");
+		requestDispatcher.forward(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// metodo post
+	}
+
+}
