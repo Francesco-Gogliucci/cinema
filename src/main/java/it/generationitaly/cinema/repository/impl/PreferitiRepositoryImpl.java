33a1cb082fc9df888788cd424b05194003561819
Francesco <fgogliucci@gmail.com>
2024-10-11 09:29:31 +0200

    costruttore preferiti impl

--- src/main/java/it/generationitaly/cinema/repository/impl/PreferitiRepositoryImpl.java
+++ src/main/java/it/generationitaly/cinema/repository/impl/PreferitiRepositoryImpl.java
@@ -4,5 +4,7 @@ import it.generationitaly.cinema.entity.Preferiti;
 import it.generationitaly.cinema.repository.PreferitiRepository;
 
 public class PreferitiRepositoryImpl extends JpaRepositoryImpl<Preferiti, Long> implements PreferitiRepository {
-
+	public PreferitiRepositoryImpl() {
+		super(Preferiti.class);
+	}
 }
