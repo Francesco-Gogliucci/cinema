713a29be61872a3d6246280712b38c7c5aa282fd
Francesco <fgogliucci@gmail.com>
2024-10-11 09:23:02 +0200

    fix film impl

--- src/main/java/it/generationitaly/cinema/repository/impl/FilmRepositoryImpl.java
+++ src/main/java/it/generationitaly/cinema/repository/impl/FilmRepositoryImpl.java
@@ -3,7 +3,7 @@ package it.generationitaly.cinema.repository.impl;
 import it.generationitaly.cinema.entity.Film;
 import it.generationitaly.cinema.repository.FilmRepository;
 
-public class FilmRepositoryImpl extends JpaRepository<Film, Long> implements FilmRepository {
+public class FilmRepositoryImpl extends JpaRepositoryImpl<Film, Long> implements FilmRepository {
 
 	public FilmRepositoryImpl() {
 		super(Film.class);
