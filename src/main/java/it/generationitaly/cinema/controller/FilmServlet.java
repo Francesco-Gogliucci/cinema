ed02f1ab4e904f0221a148a7845e3fa4cddcdcc6
Sunshine <Sunshine@DESKTOP-UBTQAOS.lan>
2024-10-11 09:41:45 +0200

    film sevlet vuota

---
+++ src/main/java/it/generationitaly/cinema/controller/FilmServlet.java
@@ -0,0 +1,25 @@
+package it.generationitaly.cinema.controller;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+public class FilmServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
