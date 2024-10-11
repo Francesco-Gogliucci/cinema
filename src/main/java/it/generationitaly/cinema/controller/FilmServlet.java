428ef0d67225626d73465a0f529f457f7b19a947
Francesco <fgogliucci@gmail.com>
2024-10-11 16:58:58 +0200

    aggiunte le recensioni quando si visualizzano i dettagli di un film

--- src/main/java/it/generationitaly/cinema/controller/FilmServlet.java
+++ src/main/java/it/generationitaly/cinema/controller/FilmServlet.java
@@ -5,8 +5,10 @@ import java.util.List;
 
 import it.generationitaly.cinema.entity.Attore;
 import it.generationitaly.cinema.entity.Film;
+import it.generationitaly.cinema.entity.Recensione;
 import it.generationitaly.cinema.repository.impl.FilmAttoreRepositoryImpl;
 import it.generationitaly.cinema.repository.impl.FilmRepositoryImpl;
+import it.generationitaly.cinema.repository.impl.RecensioneRepositoryImpl;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -17,6 +19,8 @@ public class FilmServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	private FilmRepositoryImpl filmRepository = new FilmRepositoryImpl();
+	private FilmAttoreRepositoryImpl filmAttoreRepository = new FilmAttoreRepositoryImpl();
+	private RecensioneRepositoryImpl recensioneRepository = new RecensioneRepositoryImpl();
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -25,10 +29,12 @@ public class FilmServlet extends HttpServlet {
 		Film film = filmRepository.findById(id);
 		request.setAttribute("film", film);
 
-		FilmAttoreRepositoryImpl filmAttoreRepository = new FilmAttoreRepositoryImpl();
 		List<Attore> attori = filmAttoreRepository.findAttoreByFilmId(id);
-
 		request.setAttribute("attori", attori);
+
+		List<Recensione> recensioni = recensioneRepository.findRecensioneByFilmId(id);
+		request.setAttribute("recensioni", recensioni);
+
 		// inserire la pagina jsp corretta per la visualizzazione dei dettagli di un
 		// film
 		RequestDispatcher requestDispatcher = request.getRequestDispatcher("film.jsp");
--- src/main/java/it/generationitaly/cinema/repository/RecensioneRepository.java
+++ src/main/java/it/generationitaly/cinema/repository/RecensioneRepository.java
@@ -1,9 +1,11 @@
 package it.generationitaly.cinema.repository;
 
+import java.util.List;
+
 import it.generationitaly.cinema.entity.Recensione;
 
 public interface RecensioneRepository extends JpaRepository<Recensione, Long> {
-	
-	
-	
+
+	public List<Recensione> findRecensioneByFilmId(Long filmId);
+
 }
