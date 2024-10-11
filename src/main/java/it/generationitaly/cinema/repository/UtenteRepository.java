4598eafb104b1745cf88aeb92c193c48c39112b6
laura <laura@Tequi_Tom>
2024-10-11 10:15:07 +0200

    Login , utente repository e impl

--- src/main/java/it/generationitaly/cinema/repository/UtenteRepository.java
+++ src/main/java/it/generationitaly/cinema/repository/UtenteRepository.java
@@ -4,4 +4,5 @@ import it.generationitaly.cinema.entity.Utente;
 
 public interface UtenteRepository extends JpaRepository<Utente, Long> {
 
+	Utente findByUsername (String username);
 }
--- src/main/java/it/generationitaly/cinema/repository/impl/UtenteRepositoryImpl.java
+++ src/main/java/it/generationitaly/cinema/repository/impl/UtenteRepositoryImpl.java
@@ -1,10 +1,43 @@
 package it.generationitaly.cinema.repository.impl;
 
+import java.util.List;
+
 import it.generationitaly.cinema.entity.Utente;
 import it.generationitaly.cinema.repository.UtenteRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.TypedQuery;
 
 public class UtenteRepositoryImpl extends JpaRepositoryImpl<Utente, Long> implements UtenteRepository {
 	public UtenteRepositoryImpl() {
 		super(Utente.class);
 	}
+
+	@Override
+	public Utente findByUsername(String username) {
+		Utente utente = null;
+		EntityManager em = null;
+		EntityTransaction tx = null;
+		try {
+			em = emf.createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+			TypedQuery<Utente> query = em.createQuery("SELECT u FROM Utente u WHERE u.username = :username",
+					Utente.class);
+			query.setParameter("username", username);
+			List<Utente> utenti = query.getResultList();
+			utente = utenti.isEmpty() ? null : utenti.get(0);
+			tx.commit();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+			if (tx != null && tx.isActive())
+				tx.rollback();
+		} finally {
+			if (em != null)
+				em.close();
+		}
+		return utente;
+	}
+
 }
+
