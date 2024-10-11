160f72bd7f9067ee983d25f6895400e9ef22f3ed
Angelo <Angelo@DESKTOP-9FSKDUU.station>
2024-10-11 11:58:58 +0200

    aggiunto findpref

--- src/main/java/it/generationitaly/cinema/repository/UtenteRepository.java
+++ src/main/java/it/generationitaly/cinema/repository/UtenteRepository.java
@@ -1,10 +1,11 @@
 package it.generationitaly.cinema.repository;
-
+import it.generationitaly.cinema.entity.Film;
 import it.generationitaly.cinema.entity.Utente;
-
+import java.util.List;
 public interface UtenteRepository extends JpaRepository<Utente, Long> {
 
 
 	Utente findByUsername (String username);
 	boolean existsByUsername(String username);
+	public List<Film> findPreferitiByUtenteId(Long utenteId);
 }
