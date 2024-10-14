e5e3bd8b3a55805dbd7b74daf46dbf0d0a9f0b25
Francesco <fgogliucci@gmail.com>
2024-10-14 14:08:09 +0200

    ricerca attore per nome/cognome

--- src/main/java/it/generationitaly/cinema/repository/AttoreRepository.java
+++ src/main/java/it/generationitaly/cinema/repository/AttoreRepository.java
@@ -6,6 +6,8 @@ import it.generationitaly.cinema.entity.Attore;
 import it.generationitaly.cinema.entity.Film;
 
 public interface AttoreRepository extends JpaRepository<Attore, Long> {
-	
+
 	List<Film> findListFilmByAttore(long id);
+
+	List<Attore> findAttoreByNomeECognome(String nome, String cognome);
 }
--- src/main/java/it/generationitaly/cinema/repository/impl/AttoreRepositoryImpl.java
+++ src/main/java/it/generationitaly/cinema/repository/impl/AttoreRepositoryImpl.java
@@ -23,18 +23,43 @@ public class AttoreRepositoryImpl extends JpaRepositoryImpl<Attore, Long> implem
 			em = emf.createEntityManager();
 			tx = em.getTransaction();
 			tx.begin();
-			TypedQuery<Film> query = em.createQuery("SELECT f FROM FilmAttore fa JOIN fa.film f JOIN fa.attore a WHERE a.id = :id", Film.class);
+			TypedQuery<Film> query = em.createQuery(
+					"SELECT f FROM FilmAttore fa JOIN fa.film f JOIN fa.attore a WHERE a.id = :id", Film.class);
 			query.setParameter("id", id);
 			listAttoreFilm = query.getResultList();
 			tx.commit();
 		} catch (Exception e) {
 			System.err.println(e.getMessage());
-			if (tx != null && tx.isActive())
+			if (tx != null && tx.isActive()) {
 				tx.rollback();
+			}
 		} finally {
-			if (em != null)
+			if (em != null) {
 				em.close();
+			}
 		}
 		return listAttoreFilm;
 	}
+
+	@Override
+	public List<Attore> findAttoreByNomeECognome(String nome, String cognome) {
+		List<Attore> attori = null;
+		String jpql = "SELECT a FROM Attore a WHERE LOWER(a.nome) LIKE LOWER(:nome) AND LOWER(a.cognome) LIKE LOWER(:cognome)";
+		EntityManager em = null;
+		try {
+			em = emf.createEntityManager();
+			TypedQuery<Attore> query = em.createQuery(jpql, Attore.class);
+			query.setParameter("nome", "%" + nome + "%");
+			query.setParameter("cognome", "%" + cognome + "%");
+			attori = query.getResultList();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+		return attori;
+	}
+
 }
